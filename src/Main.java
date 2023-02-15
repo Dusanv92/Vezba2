4bc6a9d013a296051aeeaf29d5efdd3dd365c98c
DusanV92 <dusanvucinic.dv@gmail.com>
2023-02-15 13:41:06 +0100

    Intial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.io.Console;
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args) {
+        int a= 2, b=3;
+
+        int res1 = a * (++b) + b;
+
+        System.out.println(res1);
+    }
+}
+
